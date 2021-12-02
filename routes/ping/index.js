0e9d31d5bd13c06bab28407d475268b19df6d6d0
Sergius92739 <sergem279@gmail.com>
2021-12-02 16:49:56 +0300

    initial commit

--- routes/ping/index.js
+++ routes/ping/index.js
@@ -2,7 +2,7 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/', async (ctx) => {
-  ctx.response.body = 'Серевер в порядке';
+  ctx.response.body = 'Сервер в порядке';
 });
 
 module.exports = router;
