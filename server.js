3b7537abd319dc5d01d12676e3fe5e8a939532a9
Sergius92739 <sergem279@gmail.com>
2024-01-03 14:51:50 +0300

    port fixed

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors({
 
 app.use(router());
 
-const port = process.env.PORT || 8086;
+const port = process.env.PORT || 8000;
 const server = http.createServer(app.callback());
 
 server.listen(port);
